a7b61d021bf6467cc611c2cba037cdfa48a2225c
ramonsilva186 <ramonsilva186@gmail.com>
2024-02-05 10:37:50 -0300

    Initial commit

---
+++ src/main/java/com/ramonsilva186/dev/DevWeekSantanderApplication.java
@@ -0,0 +1,13 @@
+package com.ramonsilva186.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeekSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonsilva186/dev/DevWeekSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonsilva186.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

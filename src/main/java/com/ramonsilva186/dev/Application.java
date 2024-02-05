1149c4f1f23f84f0970ead81634ff22d135aaa7b
ramonsilva186 <ramonsilva186@gmail.com>
2024-02-05 10:59:44 -0300

    change Application

--- src/main/java/com/ramonsilva186/dev/Application.java
+++ src/main/java/com/ramonsilva186/dev/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevWeekSantanderApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevWeekSantanderApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
